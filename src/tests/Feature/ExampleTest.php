25e8c09a25ede30b5e795b40ebdf6672db64f458
Kenya Ishizuka <ken8116@icloud.com>
2022-10-08 21:36:45 +0900

    first commit

---
+++ src/tests/Feature/ExampleTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_the_application_returns_a_successful_response()
+    {
+        $response = $this->get('/');
+
+        User::factory(100)->create();
+
+        $response->assertStatus(200);
+    }
+}
