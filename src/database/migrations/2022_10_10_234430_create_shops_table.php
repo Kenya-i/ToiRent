75b380b0f6ee5cd87e5ad9f83e9446c8ea5d64e6
Kenya Ishizuka <ken8116@icloud.com>
2022-10-10 23:52:22 +0900

    UserモデルとShopモデルの作成

--- src/database/migrations/2022_10_10_234430_create_shops_table.php
+++ src/database/migrations/2022_10_10_234430_create_shops_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('shops', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('age');
-            $table->tinyInteger('gender');
+            $table->string('address');
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -35,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('shops');
     }
 };
